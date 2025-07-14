6e4b6bdf02104f30fe4033af8e925ca02f56d5e5
Carlos Emanuel Klema <carlosklema@hotmail.com>
2025-07-14 11:29:01 -0300

    fix: update react-router-dom version and correct Firebase appId and measurementId

--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -9,8 +9,8 @@ const firebaseConfig = {
   projectId: "circula-4cd88",
   storageBucket: "circula-4cd88.firebasestorage.app",
   messagingSenderId: "952928794233",
-  appId: "1:952928794233:web:6720cca6163cd4786dc6a8",
-  measurementId: "G-7D15WZSEFX"
+  appId: "1:952928794233:web:f6e74d53823176e96dc6a8",
+  measurementId: "G-DPSBHFGL63"
 };
 
 const app = initializeApp(firebaseConfig);
