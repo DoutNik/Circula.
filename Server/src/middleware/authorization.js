18cb0bf1479158c0ec51be19a18b3f2289d06dd1
Carlos Emanuel Klema <carlosklema@hotmail.com>
2025-10-10 10:49:00 -0300

    refactor: remove debug log for received JWT token in authorization middleware

--- Server/src/middleware/authorization.js
+++ Server/src/middleware/authorization.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 module.exports = async (req, res, next) => {
   try {
     const jwtToken = req.header("token");
-    console.log("Token recibido:", jwtToken?.slice(0, 30)); // primeras letras
 
     if (!jwtToken) {
       return res.status(403).json("Not Authorize - No token");
