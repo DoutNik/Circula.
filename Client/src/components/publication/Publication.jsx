43cf0d6c62e12ed62633bc5ca4e90c7b39c046b2
Carlos Emanuel Klema <carlosklema@hotmail.com>
2026-03-07 23:29:49 -0300

    refactor: simplify Publication component by removing unused state and handlers, and improve CSS layout

--- Client/src/components/publication/Publication.jsx
+++ Client/src/components/publication/Publication.jsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllPosts, selectedPost, deletePost } from "../../redux/actions";
+import { getAllPosts, deletePost } from "../../redux/actions";
 
 import style from "./Publication.module.css";
-import icon from "../../assets/iconChoosed.png";
 
 const Publication = ({ userData }) => {
   const dispatch = useDispatch();
   const allPosts = useSelector((state) => state.allPostsCopy);
   const matches = useSelector((state) => state.matches);
   const [userPosts, setUserPosts] = useState([]);
-  const [selectedPostId, setSelectedPostId] = useState(null);
 
   useEffect(() => {
     dispatch(getAllPosts());
@@ -23,25 +21,6 @@ const Publication = ({ userData }) => {
       setUserPosts(filteredUserPosts);
     }
   }, [userData, allPosts]);
-
-  useEffect(() => {
-    const storedSelectedPostId = localStorage.getItem("selectedPostId");
-    if (storedSelectedPostId) {
-      setSelectedPostId(Number(storedSelectedPostId));
-    }
-  }, []);
-
-  const handlePostClick = (postId, postImage) => {
-    if (selectedPostId === postId) {
-      localStorage.removeItem("selectedPostId");
-      setSelectedPostId(null);
-      dispatch(selectedPost(null, null));
-    } else {
-      localStorage.setItem("selectedPostId", postId.toString());
-      setSelectedPostId(postId);
-      dispatch(selectedPost(postId, postImage));
-    }
-  };
 
   const handlePostDelete = async (postId) => {
     try {
@@ -75,21 +54,7 @@ return (
           />
         )}
         {post.title && <h3>{post.title}</h3>}
-        <button
-          className={style.choose}
-          onClick={() => handlePostClick(post.id, post.image[0])}
-        >
-          {selectedPostId === post.id ? (
-            <img width="24" height="24" src={icon} alt="Choose" />
-          ) : (
-            <img
-              width="24"
-              height="24"
-              src="https://img.icons8.com/color/48/circled.png"
-              alt="circled"
-            />
-          )}
-        </button>
+
         <button
           className={style.trash}
           onClick={() => handlePostDelete(post.id)}
